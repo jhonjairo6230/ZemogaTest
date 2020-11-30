e18b8cd142aad7e5eeba6320c3a9529e7edadea2
Jhon Jairo Ibarra Samboni <jhonjairo6230@gmail.com>
2020-11-29 23:34:03 -0500

    skeleton of update profile

--- FrontEnd/src/app/components/main/main.component.ts
+++ FrontEnd/src/app/components/main/main.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { MainService } from 'src/app/services/main.service';
 
 @Component({
@@ -10,12 +11,16 @@ import { MainService } from 'src/app/services/main.service';
 export class MainComponent implements OnInit, AfterViewInit {
   public profileInfo:any;
 
-  constructor(private mainService:MainService) { 
+  constructor(private mainService:MainService,private router: Router, private route: ActivatedRoute,) { 
     
   }
 
   async ngOnInit() {
     this.profileInfo =   await this.mainService.getProfileInfo("ffe1a040-3219-11eb-bbe0-c5fc6762aab8");
+  }
+
+  goToEdit(){
+    this.router.navigate(['/update-create-profile']);
   }
 
   async ngAfterViewInit() {
--- FrontEnd/src/app/components/update-create-profile/update-create-profile.component.ts
+++ FrontEnd/src/app/components/update-create-profile/update-create-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update-create-profile',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-create-profile.component.scss']
 })
 export class UpdateCreateProfileComponent implements OnInit {
-
-  constructor() { }
+  formulario: FormGroup;
+  constructor(    private router: Router,
+    private activatedRoute: ActivatedRoute,) { }
 
   ngOnInit(): void {
+  }
+
+
+   fijarFormulario(data: any) {
+    this.formulario = new FormGroup({
+      description: new FormControl(data.description, [Validators.required]),
+      image_url: new FormControl(data.image_url, [Validators.required]),
+      twitter_user_name: new FormControl(data.twitter_user_name, [Validators.required]),
+      title: new FormControl(data.title, [Validators.required]),
+      id: new FormControl(null, [Validators.required])
+    });
+  }
+
+  validateForm(){
+
+  }
+
+  edit(){}
+
+  cancel(){
+    this.router.navigate(['/main']);
   }
 
 }
